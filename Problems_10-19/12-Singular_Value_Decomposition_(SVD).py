9f867bdec7700e179bf726d947f9bde49e1e0f1f
J7P16 <johncapocyan@gmail.com>
2025-12-07 00:24:13 -0600

    added solution to problem 12

---
+++ Problems_10-19/12-Singular_Value_Decomposition_(SVD).py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+def svd_2x2_singular_values(A: np.ndarray) -> tuple:
+    A = np.array(A)
+    d, v = jacobi_rotation((A.T).dot(A))
+    s = np.sqrt([d[0][0], d[1][1],]) 
+    s_inv = np.array([ 
+        [1 / s[0],0],
+        [0, 1/s[1]]])
+    u = A.dot(v).dot(s_inv)
+    return u, s, v.T
+
+def jacobi_rotation(B: np.ndarray) -> tuple:
+    if (B[0][0] == B[1][1]):
+        theta = np.pi / 4
+        r = np.array([ 
+            [np.cos(theta), -np.sin(theta)],
+            [np.sin(theta), np.cos(theta)]])
+        r_T = r.T
+        d = r_T.dot(B).dot(r)
+    else:
+        theta = (np.arctan2((2 * B[0][1]), (B[0][0] - B[1][1]))) / 2
+        r = np.array([ 
+            [np.cos(theta), -np.sin(theta)],
+            [np.sin(theta), np.cos(theta)]])
+        r_T = r.T
+        d = r_T.dot(B).dot(r)
+
+    return np.round(d, 4), r
