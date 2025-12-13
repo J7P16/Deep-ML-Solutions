5e0ae289f5adc921ee135448e33d8bd43a7338df
J7P16 <johncapocyan@gmail.com>
2025-12-12 22:10:57 -0600

    refactor variables

--- Problems_30-39/39-Implementation_of_Log_Softmax_Function.py
+++ Problems_30-39/39-Implementation_of_Log_Softmax_Function.py
@@ -4,10 +4,10 @@ def log_softmax(scores: list) -> np.ndarray:
 	# Your code here
     maxScore = max(scores)
     probabilities = []
-    summation = 0
+    summation_term = 0
     for score in scores:
-        summation += np.exp(score - maxScore)
+        summation_term += np.exp(score - maxScore)
     for score in scores:
-        log_softmax_output = score - maxScore - np.log(summation)
+        log_softmax_output = score - maxScore - np.log(summation_term)
         probabilities.append(log_softmax_output)
     return np.round(np.array(probabilities), 4)
